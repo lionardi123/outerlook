288c4506368264cae82eb264d12cdb3dab5a6262
malingkolor <lionardi123@gmail.com>
2016-12-21 21:44:50 +0700

    PANJUL JOIN THE WAR 0.1

---
+++ resources/views/index.blade.php
@@ -0,0 +1,97 @@
+@extends('layouts.app')
+
+@section('content')
+  <div id="index-banner" class="parallax-container valign-wrapper center">
+      <div class="background">
+        <img src="/outerlook/resources/assets/image/OUTERLOOK-12.jpg">
+      </div>
+    <div class="dropdown-container">
+        <form class="height-medium">
+          <select class="valign dropdown ">
+            <option class="option-items" value="volvo">Makeup Artist</option>
+            <option value="saab">Makeup Class</option>
+            <option value="mercedes">Workshop</option>
+          </select>
+           <select id="left" class="valign dropdown">
+            <option value="volvo">Jakarta</option>
+            <option value="saab">Tangerang</option>
+          </select>
+          <button class="button height-medium" type="submit" name="action">
+              <i class="material-icons search">search</i>
+          </button>
+      </form>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="section">
+      <div class="about-us">
+        <h1 class="center bold">About Us<span class="dot">.</span></h1>
+        <div id="about-us-text" class="half-width left">
+          <p class="flow-text italic small">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</br></br>
+          I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+        </div>
+        <div class="half-width left" id="about-us-image">
+          <img class="responsive-img" src="/outerlook/resources/assets/image/OUTERLOOK-8.jpg">
+        </div>
+      </div>
+    </div>
+  </div>
+
+   <div class="container">
+    <div class="section">
+      <div id="articles">
+        <h1 class="center bold">Articles<span class="dot">.</span></h1>
+        <div class="row">
+          <div class="col s12 m4">
+            <div class="card">
+              <div class="card-image">
+                <img src="/outerlook/resources/assets/image/article3.jpg">
+                <div class="card-titles title-opacity"><span>HOW TO BE A MAKE UP ARTIST</span><span class="read-more">Read more>></span></div>
+              </div>
+            </div>
+          </div>
+          <div class="col s12 m4">
+            <div class="card">
+              <div class="card-image">
+                <img src="/outerlook/resources/assets/image/article2.jpg">
+                <div class="card-titles title-opacity"><span>ORI/FAKE M.A.C PALETTE</span><span class="read-more">Read more>></span></div>
+              </div>
+            </div>
+          </div>
+          <div class="col s12 m4">
+            <div class="card">
+              <div class="card-image">
+                <img src="/outerlook/resources/assets/image/article1.jpg">
+                <div class="card-titles title-opacity"><span>NARS ON SALE UP TO 35%!</span><span class="read-more">Read more>></span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="contact" class="container peach full-width">
+    <div class="section">
+      <div class="form white center">
+        <h1 class="center bold">Contact<span class="dot">.</span></h1>
+        <hr>
+        <div class="contact-desc">
+          <span class="italic">www.outerlook.id</span>
+        </div>
+        <div class="contact-desc">
+          <span class="italic">0821 . 1398 . 3824</span>
+        </div>
+        <hr> 
+        <form class="contact-us left-align">
+          <input type="text" id="subject" placeholder="Name">
+          <input type="text" id="subject" placeholder="Email">
+          <input type="text" id="subject" placeholder="Subject">
+          <textarea id="textarea1" class="materialize-textarea" placeholder="Message"></textarea>
+          <input class="right btn" type="submit" value="Send">
+        </form>  
+      </div>
+    </div>
+  </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Parallax Template - Materialize</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="/outerlook/public/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="/outerlook/public/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav role="navigation" class="transparent">
+    <div class="background">
+      <img src="/outerlook/resources/assets/image/OUTERLOOK-1.jpg">
+    </div>
+    <div class="nav-wrapper container">
+      <ul class="left hide-on-med-and-down">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Categories</a></li>
+        <li><a href="#">Book Online</a></li>
+        <li><a href="#">Terms and Conditions</a></li>
+      </ul>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Sign In</a></li>
+        <li><a href="#">Register</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav peach">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Categories</a></li>
+        <li><a href="#">Book Online</a></li>
+        <li><a href="#">Terms and Conditions</a></li>
+        <li><a href="#">Sign In</a></li>
+        <li><a href="#">Register</a></li>
+        
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+
+  </nav>
+  @yield('content')
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="/outerlook/resources/assets/js/materialize.js"></script>
+  <script src="/outerlook/resources/assets/js/init.js"></script>
+
+  </body>
+</html>
