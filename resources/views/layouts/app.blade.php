a2af4997f5a096b3b5f487373f72d5fcdfc5b782
malingkolor <lionardi123@gmail.com>
2016-12-21 23:41:02 +0700

    fix head link/js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,13 +7,13 @@
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="{{{ asset('/css/materialize.min.css') }}}
-" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="{{{ asset('/css/materialize.css') }}}" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="{{{ asset('/css/style.css') }}}" type="text/css" rel="stylesheet" media="screen,projection"/>
   
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="{{{ asset('/js/materialize.min.js') }}}"></script>
-  <script src="/outerlook/resources/assets/js/init.js"></script>
+  <!--  Scripts-->
+  <script src="{{{ asset('/js/jquery.min.js') }}}"></script>
+  <script src="{{{ asset('/js/materialize.js') }}}"></script>
+  <script src="{{{ asset('/js/init.js') }}}"></script>
   <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
 </head>
 <body>
