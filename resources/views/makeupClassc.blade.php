45a979363f543dcd40d1079d1674e516d505ac70
malingkolor <lionardi123@gmail.com>
2016-12-22 14:35:51 +0700

    categories 0.1

--- resources/views/makeupClassc.blade.php
+++ resources/views/makeupClassc.blade.php
@@ -10,13 +10,52 @@
     <div class="parallax"><img src="{{{asset('image/category-header1.jpg')}}}" alt="Unsplashed background img 1"></div>
   </div>
   <div class="categories-container peach">
-  	<div class="side-bar white">
-	asdas
-  	</div>
-  	<div class="row categories-cards">
-  		<div class="col s3 offset-s3">
-  		asdas
-  		</div>
+  	<div class="categories-cards">
+    <?php 
+      for ($x = 0; $x <= 3; $x++) { ?>
+         <div class="row">
+        <div class="col s12 m3 l3 offset-m3 offset-l3">
+          <div class="card small-card">
+            <div class="card-image">
+              <img class="max-size" src="{{{asset('/image/makeup-class1.jpg')}}}">
+            </div>
+            <div class="card-content">
+              <div class="card-profile">
+                <img class="responsive-img" src="{{{asset('/image/round-profile1.png')}}}">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m3 l3">
+          <div class="card small-card">
+            <div class="card-image">
+              <img class="max-size" src="{{{asset('/image/makeup-class2.jpg')}}}">
+            </div>
+            <div class="card-content">
+              <div class="card-profile">
+                <img class="responsive-img" src="{{{asset('/image/round-profile1.png')}}}">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col s12 m3 l3">
+          <div class="card small-card">
+            <div class="card-image max-size">
+              <img class="max-size" src ="{{{asset('/image/makeup-class3.jpg')}}}">
+            </div>
+            <div class="card-content">
+              <div class="card-profile">
+                <img class="responsive-img" src="{{{asset('/image/round-profile1.png')}}}">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <?php
+      } 
+    ?>
+     
   	</div>
   </div>
 @endsection
