930bee3e6125dc2d3d8d77f7182da4d94dfba677
malingkolor <lionardi123@gmail.com>
2016-12-22 19:36:32 +0700

    Sign in modal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,23 @@
     <script src="{{{ asset('/js/jquery.min.js') }}}"></script>
     <script src="{{{ asset('/js/materialize.min.js') }}}"></script>
     <script src="{{{ asset('/js/init.js') }}}"></script>
+    <!-- Modal Structure -->
+    <div id="modal1" class="modal peach">
+        <form action="#" method="POST">
+            <div class="modal-content">
+                <h3 class="center">Sign In</h3>
+                <div class="row">
+                    <div class="col s12">
+                        <input type="text" id="email" placeholder="Email">               
+                        <input type="text" id="password" placeholder="Password">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer peach">
+                <button type="submit" class="modal-action modal-close waves-effect waves-green btn-flat">Sign In</button>
+            </div>
+        </form>
+    </div>
     <nav role="navigation" class="transparent">
         <div class="nav-wrapper">
             <div class="background">
@@ -38,9 +55,9 @@
                     </li>
                 </ul>
                 <ul class="right hide-on-med-and-down">
-                    <li><a href="#">Sign In</a>
+                    <li><a href="#modal1">Sign In</a>
                     </li>
-                    <li><a href="#">Register</a>
+                    <li><a href="#modal1">Register</a>
                     </li>
                 </ul>
 
@@ -57,13 +74,27 @@
                     </li>
                     <li><a href="#">Register</a>
                     </li>
-
                 </ul>
                 <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
             </div>
         </div>
     </nav>
     @yield('content')
+    <script>
+        $('.modal').modal({
+                  dismissible: true, // Modal can be dismissed by clicking outside of the modal
+                  opacity: .5, // Opacity of modal background
+                  in_duration: 300, // Transition in duration
+                  out_duration: 200, // Transition out duration
+                  starting_top: '4%', // Starting top style attribute
+                  ending_top: '10%', // Ending top style attribute
+                  ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+                    console.log(modal, trigger);
+                },
+            }
+        );
+      
+    </script>
 </body>
 
 </html>
